4e8907f748d672fd7e66e3dd7eae392ccd56fe33
soulhidee <serverpgig@yandex.ru>
2025-05-31 18:39:33 +0300

    feat: Add UI elements for profile image, labels, text field and button

--- EasyToDo/RegistrationViewController.swift
+++ EasyToDo/RegistrationViewController.swift
@@ -1,7 +1,13 @@
 import UIKit
 
 final class RegistrationViewController: UIViewController {
-
+    private let profileImageView = UIImageView()
+    private let helloLabel = UILabel()
+    private let greetingLabel = UILabel()
+    private let nameTextField = UITextField()
+    private let startButton = UIButton()
+    private let settingsHintLabel = UILabel()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
