fded03295edd83d38f9bfbee4d8ef7c64dbccc9e
soulhidee <serverpgig@yandex.ru>
2025-05-31 14:21:59 +0300

    chore: Init project and add .gitignore

---
+++ EasyToDo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EasyToDo
+//
+//  Created by Даниил on 31.05.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
