bd7c608ff473fe8506039d6395a3ae522824c502
soulhidee <serverpgig@yandex.ru>
2025-06-02 17:21:23 +0300

    refactor: Move constraints to a separate file

--- EasyToDo/UIComponents.swift
+++ EasyToDo/UIComponents.swift
@@ -1,3 +1,12 @@
 import UIKit
 
-struct Profile
+struct ProfileImage {
+    func makeImageView() -> UIImageView {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "ProfileImage")
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }
+}
